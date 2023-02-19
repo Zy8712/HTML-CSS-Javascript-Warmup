e9c6900863778fcba0a3cca20e70b96af482856e
Bryan Li <76718507+Zy8712@users.noreply.github.com>
2023-02-19 01:39:57 -0500

    Add files via upload

--- Practice_Round_2/lab4/task6.js
+++ Practice_Round_2/lab4/task6.js
@@ -1,1 +1,12 @@
 /* Task6.js - Add your Java Script Code Here */
+var i = 20;
+function myFunction(){
+  var p = document.getElementById("mydata");
+  if(i <= 0){
+    p.innerHTML = "BOOM!"
+  }
+  else{
+    p.innerHTML = i;
+  }
+  i--;
+}
